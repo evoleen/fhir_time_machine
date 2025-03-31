b545e9e6b7ad1520af89ba7cd2390faffcca22b5
Till Gerken <till.gerken@gmail.com>
2025-03-31 12:07:19 +0200

    Add tests

--- lib/src/fhir.dart
+++ lib/src/fhir.dart
@@ -2,40 +2,43 @@ import 'package:fhir/r4.dart';
 import 'package:time_machine2/time_machine2.dart';
 
 extension FhirInstantX on FhirInstant {
-  /// Converts a [FhirDateTime] to an [Insant] using the FHIR date time pattern.
+  /// Converts a [FhirDateTime] to an [Instant] using the FHIR date time pattern.
   ///
   /// Throws a [FormatException] if:
   /// - The input string doesn't match any of the expected formats
   /// - The timestamp doesn't include a time zone specification
   Instant toInstant() {
+    // try with milliseconds first
     try {
       final pattern = OffsetDateTimePattern.createWithInvariantCulture(
-        "uuuu-MM-dd'T'HH:mm:ss[.FFF]<O>",
+        "uuuu-MM-dd'T'HH:mm:ss.fffo<G>",
       );
-
-      if (!toString().contains('+') &&
-          !toString().contains('-') &&
-          !toString().contains('Z')) {
-        throw FormatException(
-          'Timestamp must include a time zone offset or UTC "Z" specification.',
-        );
-      }
 
       final offsetDateTime = pattern.parse(toString()).value;
       return offsetDateTime.toInstant();
     } catch (e) {
-      if (e is FormatException) {
-        rethrow;
+      // try with seconds only
+      try {
+        final pattern = OffsetDateTimePattern.createWithInvariantCulture(
+          "uuuu-MM-dd'T'HH:mm:sso<G>",
+        );
+
+        final offsetDateTime = pattern.parse(toString()).value;
+        return offsetDateTime.toInstant();
+      } catch (e) {
+        if (e is FormatException) {
+          rethrow;
+        }
+        throw FormatException(
+          'Failed to parse "$valueString". Input must include a valid time zone specification and match one of the supported formats.',
+        );
       }
-      throw FormatException(
-        'Failed to parse "$valueString". Input must include a valid time zone specification and match one of the supported formats.',
-      );
     }
   }
 }
 
 extension FhirDateTimeX on FhirDateTime {
-  /// Converts a [FhirDateTime] to an [Insant] using the FHIR date time pattern.
+  /// Converts a [FhirDateTime] to an [Instant] using the FHIR date time pattern.
   /// It first attempts to parse the string using the extended ISO 8601 format.
   /// If unsuccessful, it will try the general ISO 8601 format.
   /// If both fail, it will try the FHIR specific format. The FHIR time stamp
--- test/fhir_time_machine_test.dart
+++ test/fhir_time_machine_test.dart
@@ -39,5 +39,52 @@ void main() {
       expect(instant, isA<Instant>());
       expect(instant, Instant.utc(2021, 1, 1, 1, 0));
     });
+
+    test('toInstant() converts timestamp with milliseconds correctly', () {
+      final dateTime = FhirDateTime('2021-01-01T00:00:00.123+00:00');
+      final instant = dateTime.toInstant();
+      expect(instant, isA<Instant>());
+      expect(
+        instant,
+        Instant.utc(2021, 1, 1, 0, 0, 0).add(Time(milliseconds: 123)),
+      );
+    });
+  });
+
+  group('FhirInstantX', () {
+    test('toFhirDateTime() converts instant with Z offset correctly', () {
+      final fhirInstant = FhirInstant('2021-01-01T00:00:00Z');
+      final instant = fhirInstant.toInstant();
+      expect(instant, isA<Instant>());
+      expect(instant, Instant.utc(2021, 1, 1, 0, 0));
+    });
+
+    test('toFhirDateTime() converts instant with +00:00 offset correctly', () {
+      final fhirInstant = FhirInstant('2021-01-01T00:00:00+00:00');
+      final instant = fhirInstant.toInstant();
+      expect(instant, isA<Instant>());
+      expect(instant, Instant.utc(2021, 1, 1, 0, 0));
+    });
+
+    test('toFhirDateTime() converts instant with -00:00 offset correctly', () {
+      final fhirInstant = FhirInstant('2021-01-01T00:00:00-00:00');
+      final instant = fhirInstant.toInstant();
+      expect(instant, isA<Instant>());
+      expect(instant, Instant.utc(2021, 1, 1, 0, 0));
+    });
+
+    test('toFhirDateTime() converts instant with +01:00 offset correctly', () {
+      final fhirInstant = FhirInstant('2021-01-01T00:00:00+01:00');
+      final instant = fhirInstant.toInstant();
+      expect(instant, isA<Instant>());
+      expect(instant, Instant.utc(2020, 12, 31, 23, 0));
+    });
+
+    test('toFhirDateTime() converts instant with -01:00 offset correctly', () {
+      final fhirInstant = FhirInstant('2021-01-01T00:00:00-01:00');
+      final instant = fhirInstant.toInstant();
+      expect(instant, isA<Instant>());
+      expect(instant, Instant.utc(2021, 1, 1, 1, 0));
+    });
   });
 }
